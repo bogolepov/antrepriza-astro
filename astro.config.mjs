e37ae9c302c30eb8b71f59e917c3fa3d8ddba850
Vadim Bogolepov <bogolepov@gmail.com>
2024-04-26 04:59:39 +0200

    AE-1. Bug fixed
    Titel and icon added to the root page index.html

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
-		defaultLocale: 'ru',
-		locales: ['de', 'ru'],
-		routing: {
-			prefixDefaultLocale: false,
-			redirectToDefaultLocale: true,
-		},
-		fallback: {
-			de: 'ru',
-		},
-	},
 	integrations: [react()],
 	redirects: {
 		'/afisha/': '/ru/programm/',
