7b113e566be0ff9067453e27a94086278da04ca5
Vadim Bogolepov <bogolepov@gmail.com>
2024-05-21 18:36:51 +0200

    1. Career page added 2. Reservation emails to client and to Antrepriza - done!

--- netlify/functions/sendMail.js
+++ netlify/functions/sendMail.js
@@ -30,20 +30,34 @@ export const handler = async (event, context) => {
 	const messageData = JSON.parse(event.body);
 	const { lang, name, email, reservations, amount } = messageData;
 
-	const htmlEmailClient = makeHtmlForClient(lang, name, email, reservations, amount);
+	let htmlHead, htmlReservationsTable;
+	const htmlEmailToClient = makeHtmlEmail(true, lang, name, email, reservations, amount);
+	const htmlEmailToAntrepriza = makeHtmlEmail(false, lang, name, email, reservations, amount);
 	// console.log(htmlEmailClient);
 
-	const mailOptions = {
-		from: `${theater.longTheaterName[lang]} <${process.env.ANTREPRIZA_EMAIL}>`,
-		to: email,
-		subject: `${dictionaryServer.email_ticket_subject[lang]}`,
-		text: `Hello, ${name}!`,
-		html: htmlEmailClient,
+	const makeMailOptions = isForClient => {
+		return {
+			from: `${theater.longTheaterName[lang]} <${process.env.ANTREPRIZA_EMAIL}>`,
+			to: isForClient ? email : process.env.ANTREPRIZA_TICKETS_EMAIL,
+			subject: `${dictionaryServer.email_ticket_subject[lang]}`,
+			// text: `Hello, ${name}!`,
+			html: isForClient ? htmlEmailToClient : htmlEmailToAntrepriza,
+		};
 	};
 
 	try {
 		// console.log('call sendMail.......');
-		let info = await transporter.sendMail(mailOptions);
+
+		// email for the client;
+		// the result of sending is IMPORTANT
+		await transporter.sendMail(makeMailOptions(true));
+		// email for Antrepriza
+		// the result of sending is NOT important,
+		// because the reservations were added to database
+		transporter.sendMail(makeMailOptions(false), (err, result) => {
+			console.error(err);
+		});
+		// await transporter.sendMail(makeMailOptions(false));
 
 		// console.log('result:');
 		// console.log(info);
@@ -64,92 +78,125 @@ export const handler = async (event, context) => {
 			}),
 		};
 	}
-};
 
-function makeHtmlForClient(lang, name, email, reservations, amount) {
-	return `<!DOCTYPE html><html lang="${lang}">` + makeHead(lang) + makeBody(lang, name, email, reservations, amount) + `</html>`;
-}
+	function makeHtmlEmail(isForClient, lang, name, email, reservations, amount) {
+		return (
+			`<!DOCTYPE html><html lang="${lang}">` + makeHead(lang) + makeBody(isForClient, lang, name, email, reservations, amount) + `</html>`
+		);
+	}
 
-function makeHead(lang) {
-	return (
-		'<head><meta charset="UTF-8"><title>' +
-		theater.shortTheaterName[lang] +
-		' - ' +
-		dictionaryServer.email_ticket_subject[lang] +
-		'</title><style>' +
-		'table {border-spacing:0;} td {vertical-align:top;}' +
-		'.email {width:100%;font-size:16px;background-color:#292929;color:#d6d6d6;}' +
-		'.email-wrapper {padding:2rem;margin-left:auto;margin-right:auto;} ' +
-		'.play-table {width:100%;border-top:1px solid;border-spacing:0 25px;}' +
-		'.play-titel {font-weight:700;font-size:1.8rem;line-height:1.1em;}' +
-		'.play-date {color:#87605e;font-weight:700;font-size:1.1rem;padding-bottom:0.1em;line-height:1.2em;margin-right:18px;}' +
-		'.play-info {font-size:1.1rem; font-weight:300;line-height:1.2em;padding-bottom:0.25em;}' +
-		'.tickets-table {width:100%;border-spacing:12px 0;margin-bottom:10px;margin-top:5px;text-align:right;color:#888888;}' +
-		'.tickets-name {min-width:140px;text-align:left;}' +
-		'.tickets-count {min-width:45px;} .tickets-amount {min-width:55px;}' +
-		'.total-amount {font-size:1.3rem;line-height:1.2em;padding-top:0.5em;border-top:1px solid;}' +
-		'.td-right {text-align:right;} .td-right .total-amount {padding-right:15px;}' +
-		'.address {font-size:1.15rem; line-height:1.2em;}' +
-		'</style></head>'
-	);
-}
+	function makeHead(lang) {
+		if (!!htmlHead) return htmlHead;
 
-function makeBody(lang, name, email, reservations, amount) {
-	return (
-		`<body><table class='email'><tbody><tr><td class='email-wrapper'>` +
-		makeTextAboutReservation(lang, name, email) +
-		makeReservationsTable(lang, reservations, amount) +
-		`</td></tr></tbody></table></body>`
-	);
-}
+		htmlHead =
+			'<head><meta charset="UTF-8"><title>' +
+			theater.shortTheaterName[lang] +
+			' - ' +
+			dictionaryServer.email_ticket_subject[lang] +
+			'</title><style>' +
+			'table {border-spacing:0;} td {vertical-align:top;}' +
+			'.email-body {font-size:16px;background-color:#292929;color:#d6d6d6;}' +
+			'.email-body a {color:#d6d6d6;}' +
+			'.body-table {width:100%;}' +
+			'.email-wrapper {padding:2rem;margin-left:auto;margin-right:auto;} ' +
+			'.reservation-titel {font-size:1.35rem;margin-bottom:15px;line-height:1.2em;}' +
+			'.user-table {margin-bottom:15px;}' +
+			'.user-table tr td {min-width:5em;}' +
+			'.play-table {width:100%;border-top:1px solid;border-spacing:0 25px;}' +
+			'.play-titel {font-weight:700;font-size:1.8rem;line-height:1.1em;}' +
+			'.play-date {color:#87605e;font-weight:700;font-size:1.1rem;padding-bottom:0.1em;line-height:1.2em;margin-right:18px;}' +
+			'.play-info {font-size:1.1rem; font-weight:300;line-height:1.2em;padding-bottom:0.25em;}' +
+			'.tickets-table {width:100%;border-spacing:12px 0;margin-bottom:10px;margin-top:5px;text-align:right;color:#888888;}' +
+			'.tickets-table * {line-height:1.2em;}' +
+			'.tickets-name {min-width:140px;text-align:left;padding-left:15px;}' +
+			'.tickets-count {min-width:45px;} .tickets-amount {min-width:55px;}' +
+			'.total-amount {font-size:1.3rem;line-height:1.2em;padding-top:0.5em;border-top:1px solid;}' +
+			'.td-right {text-align:right;} .td-right .total-amount {padding-right:15px;}' +
+			'.address {font-size:1.1rem; line-height:1.2em;}' +
+			'</style></head>';
 
-function makeTextAboutReservation(lang, name, email) {
-	return `<h1>Hello, ${name}</h1>`;
-}
+		return htmlHead;
+	}
 
-function makeReservationsTable(lang, reservations, amount) {
-	// console.log(reservations);
+	function makeBody(isForClient, lang, name, email, reservations, amount) {
+		return (
+			`<body class='email-body'><table class='body-table'><tbody><tr><td class='email-wrapper'>` +
+			makeTextAboutReservation(isForClient, lang, name, email) +
+			makeReservationsTable(lang, reservations, amount) +
+			`</td></tr></tbody></table></body>`
+		);
+	}
 
-	let table = '<table class="play-table" cellpadding="0" cellspacing="15"><tbody>';
-	reservations.forEach(element => {
-		let thisPlay = theater.plays.find(item => item.id === element.play_id);
-		let playName = thisPlay.title[lang];
-		let playDate = new Date(element.date);
-		let options = {
-			year: 'numeric',
-			month: 'short',
-			day: '2-digit',
-		};
-		let strDate = playDate.toLocaleDateString(lang, options);
+	function makeTextAboutReservation(isForClient, lang, name, email) {
+		if (isForClient) {
+			return `<h1>Hello, ${name}</h1>`;
+		} else {
+			let dateOfReservation = new Date(Date.now());
+			let options = {
+				year: 'numeric',
+				month: 'long',
+				day: '2-digit',
+				hour: '2-digit',
+				minute: '2-digit',
+			};
+			let strCurrentDate = dateOfReservation.toLocaleDateString(lang, options);
 
-		// 📆 🎭 📍
+			return (
+				`<div class='reservation-titel'>${dictionaryServer.new_reservation_text[lang]} :</div>` +
+				`<table class='user-table'><tr><td>${dictionaryServer.lang_name[lang]} :</td><td>${name}</td></tr>` +
+				`<tr><td>Email :</td><td>${email}</td></tr>` +
+				`<tr><td>${dictionaryServer.lang_when[lang]} :</td><td>${strCurrentDate}</td></tr></table>`
+			);
+		}
+	}
 
-		table =
-			table +
-			`<tr><td><div class='play-date'>${strDate}</div><div class='play-date'>${element.time}</div></td>` +
-			`<td><div class='play-titel'>${playName.toUpperCase()}</div>` +
-			`<div class='play-info'>${thisPlay.genre[lang]}, ${thisPlay.age}, ${thisPlay.lang[lang]}</div>` +
-			`<table class='tickets-table'>`;
+	function makeReservationsTable(lang, reservations, amount) {
+		if (!!htmlReservationsTable) return htmlReservationsTable;
+		// console.log(reservations);
 
-		element.tickets.forEach(ticket => {
-			if (ticket.count < 1) return;
+		let table = '<table class="play-table" cellpadding="0" cellspacing="15"><tbody>';
+		reservations.forEach(element => {
+			let thisPlay = theater.plays.find(item => item.id === element.play_id);
+			let playName = thisPlay.title[lang];
+			let playDate = new Date(element.date);
+			let options = {
+				year: 'numeric',
+				month: 'short',
+				day: '2-digit',
+			};
+			let strDate = playDate.toLocaleDateString(lang, options);
 
-			let ticketType = theater.prices.find(price => price.type === ticket.type);
+			// 📆 🎭 📍
+
 			table =
 				table +
-				`<tr><td class='tickets-name'>${ticketType.text[lang]}</td><td>${ticketType.value}€</td>` +
-				`<td>${ticket.count}${dictionaryServer.lang_count[lang]}</td>` +
-				`<td class='tickets-amount'>${ticketType.value * ticket.count}€</td></tr>`;
+				`<tr><td><div class='play-date'>${strDate}</div><div class='play-date'>${element.time}</div></td>` +
+				`<td><div class='play-titel'>${playName.toUpperCase()}</div>` +
+				`<div class='play-info'>${thisPlay.genre[lang]}, ${thisPlay.age}, ${dictionaryServer.play_lang[thisPlay.lang_id][lang]}</div>` +
+				`<table class='tickets-table'>`;
+
+			element.tickets.forEach(ticket => {
+				if (ticket.count < 1) return;
+
+				let ticketType = theater.prices.find(price => price.type === ticket.type);
+				table =
+					table +
+					`<tr><td class='tickets-name'>${ticketType.text[lang]}</td><td>${ticketType.value}€</td>` +
+					`<td>${ticket.count}${dictionaryServer.lang_count[lang]}</td>` +
+					`<td class='tickets-amount'>${ticketType.value * ticket.count}€</td></tr>`;
+			});
+
+			table = table + `</table><div class='address'>${theater.stageAddress.full_string}</div></td></tr>`;
+
+			// console.log(element);
 		});
+		table =
+			table +
+			`<tr><td><div class='total-amount'>${dictionaryServer.total_amount[lang]}</div></td>` +
+			`<td class='td-right'><div class='total-amount'>${amount}€</div></td></tr></tbody></table>`;
+		// console.log(table);
 
-		table = table + `</table><div class='address'>${theater.stageAddress.full_string}</div></td></tr>`;
-
-		// console.log(element);
-	});
-	table =
-		table +
-		`<tr><td><div class='total-amount'>${dictionaryServer.total_amount[lang]}</div></td>` +
-		`<td class='td-right'><div class='total-amount'>${amount}€</div></td></tr></tbody></table>`;
-	// console.log(table);
-	return table;
-}
+		htmlReservationsTable = table;
+		return htmlReservationsTable;
+	}
+};
