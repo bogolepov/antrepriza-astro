a9f63cf50318242e94001027f35e3d9b344c3872
bogolepov <bogolepov@gmail.com>
2024-07-20 03:59:52 +0200

    Sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	site: 'https://antrepriza.eu',
+	integrations: [
+		react(),
+		sitemap({
+			filter: page =>
+				page !== 'https://antrepriza.eu/404/' &&
+				page !== 'https://antrepriza.eu/ru/404/' &&
+				page !== 'https://antrepriza.eu/de/404/' &&
+				page !== 'https://antrepriza.eu/newsletter/',
+		}),
+	],
 	redirects: {
 		'/afisha/': '/ru/program/',
 		'/bilet/': '/ru/tickets/',
-
 		'/program/': '/ru/program/',
 		'/tickets/': '/ru/tickets/',
 		'/support/': '/ru/support/',
